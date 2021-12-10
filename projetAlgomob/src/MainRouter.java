417fbdc8d61d5e00d0e5a7df1dc27e8ca9cb2410
sarars1 <95846917+sarars1@users.noreply.github.com>
2021-12-10 17:40:05 +0100

    Add files via upload

--- projetAlgomob/src/MainRouter.java
+++ projetAlgomob/src/MainRouter.java
@@ -9,12 +9,20 @@ import io.jbotsim.ui.JTopology;
 import io.jbotsim.ui.JViewer;
 
 public class MainRouter {
-    public final static int n = 11;
+    public final static int n = 5;
     public static void main(String[] args) {
 
         Topology tp = new Topology();
         tp.setDefaultNodeModel(RouterNode.class); //
         tp.setTimeUnit(500);
+        TopologyGenerators.generateRing(tp, n);
+
+        for (int i = 0; i < n; i++){
+            RouterNode node = (RouterNode) tp.getNodes().get(i);
+            node.setLocation(node.getX()+250, node.getY()+100); // set node position
+            //node.parent = tp.getNodes().get((i+1) % n); // parent selection
+            //node.setID((int) Math.floor(Math.random() * n)); // random ID in [0..n^2[
+        }
         JTopology jtp = new JTopology(tp);
         jtp.addLinkPainter(new JParentLinkPainter()); // ajoute l'orientation
         new JViewer(jtp); // dessine la topologie
--- projetAlgomob/src/RouterNode.java
+++ projetAlgomob/src/RouterNode.java
@@ -2,7 +2,7 @@ import io.jbotsim.core.Color;
 import io.jbotsim.core.Link;
 import io.jbotsim.core.Message;
 import io.jbotsim.core.Node;
-import io.jbotsim.ui.painting.JDirectedLinkPainter;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,73 +10,123 @@ import java.util.List;
 public class RouterNode extends Node {
     Node parent = null;
     List<Node> children = new ArrayList<>();
-    private int hop = 0;
+    int hop = 1;
     Node sender;
 
+
+    // A destination node has been selected, we send a message to our neighbors and begin the routing/construction
     @Override
     public void onSelection() {
         parent = this;
         setIconSize(20);
-        sendAll(new Message("1", "CONSTRUCTION"));
+        sendAll(new Message(""+hop, "CONSTRUCTION"));
+        setColor(Color.yellow);
     }
 
     @Override
     public void onMessage(Message message) {
         sender = message.getSender();
 
-        if (message.getFlag().equals("CONSTRUCTION")) {
-            if (parent == null) {
-                parent = message.getSender();
-                getCommonLinkWith(parent).setWidth(4);
-                send(parent, new Message("", "ADOPTION"));
-                hop ++;
-                sendAll(new Message(hop, "CONSTRUCTION"));
+        int h = 0;
+        if(!message.getContent().equals("")){
+            h = Integer.parseInt(String.valueOf(message.getContent()));
+        }
 
-            }else{
-                int t = (int) message.getContent();
-                if(t<hop){
-                    //Si un sommet à déjà un pére et que le nombre de saut (hop) est plus grand que content, on envoie ELIMINATION au pere
-                    send(parent, new Message("", "ELIMINATION"));
-                    getCommonLinkWith(parent).setWidth(1);
+        switch (message.getFlag()) {
 
+            // Construction of the sub-graph - every node has a parent and is at the closest distance possible
+            // from the destination node
+            case "CONSTRUCTION":
+                // If we don't have a parent, the sender becomes our parent, and our hop it's their hop + 1
+                if (parent == null) {
                     parent = message.getSender();
-
-                    getCommonLinkWith(parent).setWidth(4);
-                    hop ++;
                     send(parent, new Message("", "ADOPTION"));
-                    sendAll(new Message(message));
-                }
-            }
-        }else if (message.getFlag().equals("ADOPTION")) {
-            children.add(message.getSender());
-        }
-        //On supprime notre ancien fils
-        else if(message.getFlag().equals("ELIMINATION")){
-            children.remove(message.getSender());
-        }
-        //Le sender à detecter un changement (une suppression), alors on envoie une MAJ à tous ses voisins
-        else if(message.getFlag().equals("DELETE")){
-            send(sender, new Message(hop, "UPDATE"));
-        }
-        // 1er cas : quand on supprime une arrete, on change de pere, et on prends le noeud le plus proche de la destination (hop le plus petit)
-        else if (message.getFlag().equals("UPDATE")){
-                if (hop >= (int) message.getContent()) {
+                    hop = h + 1;
+                    sendAll(new Message(hop, "CONSTRUCTION"));
 
-                    if (getCommonLinkWith(parent) != null) {
-                        getCommonLinkWith(parent).setWidth(1);
+                } else {
+                    // If we have a parent but the sender has smaller hop than them, if so the sender becomes our new parent, our hop becomes their hop + 1
+                    // t+1 = hop of the sender node + 1 (for us)
+                    if (h + 1 < hop) {
+
+                        send(parent, new Message("", "ABANDON"));
+                        parent = message.getSender();
+
+                        hop = h + 1;
+                        send(parent, new Message("", "ADOPTION"));
+                        sendAll(new Message(message));
                     }
 
-                    sendAll(new Message("","DELETE"));
-
-                    parent = sender;
-                    getCommonLinkWith(parent).setWidth(4);
                 }
+                break;
+
+            // A node (sender) becomes our child
+            case "ADOPTION":
+                getCommonLinkWith(message.getSender()).setWidth(4);
+                children.add(message.getSender());
+                break;
+
+            // A node (sender) is no longer our child
+            case "ABANDON":
+                getCommonLinkWith(message.getSender()).setWidth(1);
+                children.remove(message.getSender());
+                break;
+
+            // One of our neighbors detected a link removal, so we send our neighbor our hop
+            case "DELETE":
+                send(sender, new Message(hop, "UPDATE"));
+                break;
+
+            // We receive hop from our neighbors, we see if it is equal or smaller than ours,
+            // in that case the neighbor becomes our parent
+            case "UPDATE":
+                // If the neighbor's hop is smaller than ours he becomes our parent
+                if (hop > h) {
+                    send(parent, new Message("", "ABANDON"));
+                    hop = h + 1;
+                    parent = sender;
+                    send(sender, new Message("", "ADOPTION"));
+                }
+
+                // If the neighbor's hop is equal to ours, we check to see if there's a neighbor than sent us a
+                // message with a smaller hop. -> We give priority to smaller hops
+                else if (hop == h) {
+                    send(parent, new Message("", "ABANDON"));
+                    Node new_parent = sender;
+                    int new_hop = hop;
+
+                    // We get the list of messages received to check if there's a smaller than the hop we received
+                    List<Message> msgs = getMailbox();
+                    for (Message m : msgs) {
+                        if (getOutLinkTo(m.getSender()) != null && m.getFlag().equals("UPDATE")) {
+                            if ((int) m.getContent() < h) {
+                                new_parent = m.getSender();
+                                new_hop = (int) m.getContent();
+                            }
+                        }
+                    }
+                    hop = new_hop + 1;
+                    parent = new_parent;
+                    send(new_parent, new Message("", "ADOPTION"));
+                }
+                setColor(null);
+
+                break;
         }
     }
 
+    // We alert our neighbors a link has been removed
     @Override
     public void onLinkRemoved(Link link) {
         super.onLinkRemoved(link);
         sendAll(new Message("","DELETE"));
+        setColor(Color.red);
+    }
+
+    @Override
+    public String toString() {
+        return "ID= "+ getID() + " hop= " + hop ;
     }
 }
+
+
