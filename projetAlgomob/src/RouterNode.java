856d6b190cd0713959013b3deba2a1b70864b9b4
obah4567 <56270658+obah4567@users.noreply.github.com>
2021-12-20 17:06:46 +0100

    Update RouterNode.java

--- projetAlgomob/src/RouterNode.java
+++ projetAlgomob/src/RouterNode.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class RouterNode2 extends Node {
+public class RouterNode extends Node {
     Node parent = null;
     List<Node> children = new ArrayList<>();
     int hop = 0;
